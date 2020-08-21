c772a517bcc3dffba6c0ca10564b045e4fc8b9c0
sanket <sanketgargate06@gmail.com>
2020-08-21 20:06:44 +0530

    added Post component a styling

---
+++ src/posts/Post.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Post.css";
+import Avatar from "@material-ui/core/Avatar";
+
+function Post({ username, caption, imageUrl}) {
+    return (
+        <div className="post">
+            
+            {/* header -> avatar + username */}
+
+            <div className="post__header">
+                <Avatar 
+                    className="post__avatar"
+                    alt="johndoe"
+                    src=""
+                />
+                <h3>{username}</h3>
+            </div>
+
+            {/* image */}
+            
+            <img 
+             className="post__image"
+             src={imageUrl} alt=""/>
+            
+            {/* username + caption */}
+            <h4 className="post__text">
+                <strong>{username} </strong>{caption}
+            </h4>
+            
+        </div>
+    )
+}
+
+export default Post;
